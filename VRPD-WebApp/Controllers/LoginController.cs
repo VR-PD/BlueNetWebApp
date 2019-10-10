0d19237aab39c14e7beb67e1786f65d535a437ac
Bram Pieters <s1110012@student.windesheim.nl>
2019-10-10 10:26:38 +0200

    Add db connection

--- VRPD-WebApp/Controllers/LoginController.cs
+++ VRPD-WebApp/Controllers/LoginController.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Web.Mvc;
+using VRPD_WebApp.db;
 using VRPD_WebApp.Models;
 
 namespace VRPD_WebApp.Controllers
 {
     public class LoginController : Controller
     {
+        private VrpdContext db = new VrpdContext();
+
         [HttpGet]
         [AllowAnonymous]
         public ActionResult Index(string ReturnUrl)
         {
+            Guest g = db.Guest.Add(new Guest());
+            db.SaveChanges();
             if (Session[STATICS.VISITOR_KEY] == null)
             {
                 Visitor visitor = new Visitor(new Random().Next().ToString());
