0ba125f8a363b78ac979d7e4f620f079498fb300
Bram Pieters <s1110012@student.windesheim.nl>
2019-10-14 14:31:16 +0200

    Add qr code

--- VRPD-WebApp/Controllers/LoginController.cs
+++ VRPD-WebApp/Controllers/LoginController.cs
@@ -2,6 +2,9 @@
 using System.Web.Mvc;
 using VRPD_WebApp.db;
 using VRPD_WebApp.Models;
+using QRCoder;
+using System.Drawing;
+using System.IO;
 
 namespace VRPD_WebApp.Controllers
 {
@@ -9,21 +12,37 @@ namespace VRPD_WebApp.Controllers
     {
         private VrpdContext db = new VrpdContext();
 
-        [HttpGet]
         [AllowAnonymous]
-        public ActionResult Index(string ReturnUrl)
+        public ActionResult GetQrCode()
         {
             Keynum key = Session[STATICS.VISITOR_KEY] as Keynum;
-            if (key == null || (DateTime.Now - key.Created).TotalSeconds < 30)
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(key.Key, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            var bitmapBytes = BitmapToBytes(qrCodeImage); //Convert bitmap into a byte array
+            return new FileContentResult(bitmapBytes, "image/jpeg"); //Return as file result
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            #region Check key code
+
+            Keynum key = Session[STATICS.VISITOR_KEY] as Keynum;
+            if (key == null || (DateTime.UtcNow - key.Created).TotalSeconds > 30)
             {
                 Guest g = db.Guest.Add(new Guest());
                 db.SaveChanges();
-
-                Session[STATICS.VISITOR_KEY] = new Keynum(g.Keynum, g.Visited);
+                key = new Keynum(g.Keynum, g.Visited);
+                Session[STATICS.VISITOR_KEY] = key;
             }
-            ViewBag.ReturnUrl = ReturnUrl;
-            if (ReturnUrl == null)
-                ViewBag.ReturnUrl = "/";
+
+            #endregion Check key code
+
+            ViewBag.ReturnUrl = "/";
+
             return View();
         }
 
@@ -33,6 +52,22 @@ namespace VRPD_WebApp.Controllers
         {
             Session[STATICS.VISITOR_KEY] = null;
             return RedirectToActionPermanent("Index");
+        }
+
+        /// <summary>
+        /// This method is for converting bitmap into a byte array
+        /// </summary>
+        /// <param name="img"></param>
+        /// <returns></returns>
+        private static byte[] BitmapToBytes(Bitmap img)
+        {
+            byte[] buffer;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                buffer = stream.ToArray();
+            }
+            return buffer;
         }
     }
 }
