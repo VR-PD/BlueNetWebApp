a2e5a85186ac22f35a7994f9d8a6768cf6df6b01
Bram Pieters <s1110012@student.windesheim.nl>
2019-10-15 13:07:08 +0200

    Add mobile qr scanner

---
+++ VrpdScanner/VrpdScanner/Requestor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using RestSharp;
+
+namespace VrpdScanner
+{
+    public static class Requestor
+    {
+        private const string url = "https://vrpd-webapp.azurewebsites.net/api/Guests";
+
+        public static IRestResponse Send(byte[] key)
+        {
+            var client = new RestClient(url);
+            var res = client.Execute(new RestRequest(Method.POST).AddJsonBody(key));
+            return res;
+        }
+    }
+}
