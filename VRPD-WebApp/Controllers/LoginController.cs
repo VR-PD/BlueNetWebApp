6a1610b3d53493208cfa02c186f5556fc950803f
Bram Pieters <s1110012@student.windesheim.nl>
2019-10-31 11:20:01 +0100

    Clean up

--- VRPD-WebApp/Controllers/LoginController.cs
+++ VRPD-WebApp/Controllers/LoginController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Web.Mvc;
-using VRPD_WebApp.db;
-using VRPD_WebApp.Models;
-using QRCoder;
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Collections.Generic;
+using System.Web.Mvc;
+using VRPD_WebApp.db;
 using VRPD_WebApp.Utils;
 
 namespace VRPD_WebApp.Controllers
