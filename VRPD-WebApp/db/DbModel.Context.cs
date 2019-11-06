8b103e369b756a26d956e35f22ea88136cef5d65
Bram Pieters <s1110012@student.windesheim.nl>
2019-11-06 13:41:12 +0100

    NO CONNECTION STRING IN DEBUGGING POSIBLE

--- VRPD-WebApp/db/DbModel.Context.cs
+++ VRPD-WebApp/db/DbModel.Context.cs
@@ -17,7 +17,7 @@ namespace VRPD_WebApp.db
     public partial class Entities : DbContext
     {
         public Entities()
-            : base(Properties.Resources.ConnectionString ?? "name=Entities")
+            : base("name=Entities")
         {
         }
     
