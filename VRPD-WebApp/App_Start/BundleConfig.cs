48153c22700341a330af6fb358a4c3156e9a6041
Bram Pieters <s1110012@student.windesheim.nl>
2019-11-27 11:13:46 +0100

    Fix missing jQuery

--- VRPD-WebApp/App_Start/BundleConfig.cs
+++ VRPD-WebApp/App_Start/BundleConfig.cs
@@ -11,7 +11,7 @@ namespace VRPDWebApp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.4.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
