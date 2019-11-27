5506edd9e6daa7c989c5db7b3a04038cdd2f19fa
Bram Pieters <s1110012@student.windesheim.nl>
2019-11-27 10:27:40 +0100

    Back to Framework 7.2

--- VRPD-WebApp/Global.asax.cs
+++ VRPD-WebApp/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Web;
 using System.Web.Configuration;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 
@@ -34,6 +35,8 @@ namespace VRPDWebApp
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
