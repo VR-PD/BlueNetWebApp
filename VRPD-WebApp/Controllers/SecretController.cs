b292fb28e81182468b5a94f79f5cc95270cd5af7
Bram Pieters <s1110012@student.windesheim.nl>
2020-01-07 11:38:56 +0100

    Add custom error page

--- VRPD-WebApp/Controllers/SecretController.cs
+++ VRPD-WebApp/Controllers/SecretController.cs
@@ -1,45 +1,22 @@
-﻿using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web.Http;
+﻿using System.Web.Mvc;
 
 namespace BlueNetWebApp.Controllers
 {
     /// <summary>
     /// Flag for pentest to find
     /// </summary>
-    public class SecretController : ApiController
+    public class SecretController : Controller
     {
+        private const string flag = "v=-50NdPawLVY";
+
         /// <summary>
         /// Flag for pentest hidden behind Authorizer
         /// </summary>
         /// <returns></returns>
-        public IHttpActionResult GetFlag() => new Flag("v=-50NdPawLVY", Request);
-
-        private class Flag : IHttpActionResult
+        [HttpGet]
+        public ActionResult Flag()
         {
-            private string _message;
-            private HttpRequestMessage _request;
-            private HttpStatusCode _statusCode;
-
-            public Flag(string message, HttpRequestMessage request)
-            {
-                _statusCode = (HttpStatusCode)418;
-                _message = message;
-                _request = request;
-            }
-
-            public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
-            {
-                var response = new HttpResponseMessage()
-                {
-                    Content = new StringContent(_message),
-                    RequestMessage = _request,
-                    StatusCode = _statusCode
-                };
-                return Task.FromResult(response);
-            }
+            return Content(flag);
         }
     }
 }
